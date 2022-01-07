8844390834659b8916cc522a207254d9a8b91b14
Peixiang Zheng <peixiang.zheng@gmail.com>
2022-01-07 22:56:29 +0800

    重构

--- bin/mengmeng.py
+++ bin/mengmeng.py
@@ -1,50 +1,47 @@
 #!/usr/bin/env python
-#coding: utf-8
+#coding:utf-8
 
-from evdev import InputDevice
 from select import select
 import os
-
+HOME_PATH = '/home/pi/Work/raspberry_keyboard_music"
 MUSIC_PATH = '/home/pi/music'
 DEVICE_NAME = '/dev/input/event1'
-
 def log(s):
   print s
-def get_target_music():
 
-    list = os.listdir(MUSIC_PATH)
-    ret_list = []
-    for i in range(0,len(list)):
-      path = os.path.join(MUSIC_PATH,list[i])
-      if os.path.isfile(path):
-         ret_list.append(path)
-    return ret_list
+def get_music_list():
+  ''' 获得目标路径的音乐'''
+  file_list = os.listdir(MUSIC_PATH)
+  ret_list = []
+  for file_name in file_list:
+    path = os.path.join(MUSIC_PATH, file_name)
+    if os.path.isfile(path):
+       ret_list.append(path)
+  return ret_list
+
 def play_music(music_list, number):
-    index = number % len(music_list)
-    fname = music_list[index]
-    log(fname)
-    os.system("bash -x tool/player.sh %s >> tool/start.log 2>&1" % fname)
+  index = number % len(music_list)
+  fname = music_list[index]
+  log(fname)
+  os.system("bash -x %s/bin/player.sh %s >> tool/start.log 2>&1" % (HOME_PATH, fname))
 def stop_music():
-        os.system("bash -x tool/stop.sh >> tool/start.log 2>&1")
+  os.system("bash -x %s/bin/stop.sh >> tool/start.log 2>&1" % HOME_PATH)
 
-
-def loop_run(music_list):
+def loop_play_music(music_list):
+  from evdev import InputDevice
   dev = InputDevice(DEVICE_NAME)
   while True:
     select([dev], [], [])
     for event in dev.read():
-      if event.type != 1 : continue
-      if event.value != 1 : continue
+      if event.type != 1 : continue # ONLY KEYBOARD
+      if event.value != 1 : continue # ONLY PRESS
       if event.code == 1 or "%s" % event.code == "01":
         stop_music()
       else:
         log(type(event.code))
-        play_music(music_list, event.code)
-      # print "Key: %s Status: %s" % (event.code, "pressed" if event.value else "release")
-
-
+        play_music(music_list, event.code-1)
 
 
 if __name__ == '__main__':
-   music_list = get_target_music()
-   loop_run(music_list)
+   music_list = get_music_list()
+   loop_play_music(music_list)
